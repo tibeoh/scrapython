31e047507b737d2c8a55b0edd4d4e9e01b5566e1
Romain Bouyé <romain@bouye.fr>
2014-10-20 15:56:03 +0200

    New method getDomainLinks in Page

--- Page.py
+++ Page.py
@@ -1,15 +1,32 @@
+import lxml.html
+from urlparse import urlparse
+
 class Page:
-  def __init__(self, content, name, path):
+  def __init__(self, content, url):
     self.content = content
-    self.name = name
-    self.path = path
+    self.url = url
 
 
   def getUrl(self):
-    return self.path + self.name
+    return self.url
 
-  def getDomainLinks(self, max=200):
-    return
+  def getAllLinks(self, max=200):
+    dom =  lxml.html.fromstring(self.content)
+    links = []
+    for link in dom.xpath('//a/@href'): # select the url in href for all a tags(links)
+      links.append(link)
+    return links
+
+  def getDomainLinks(self):
+    links = self.getAllLinks()
+    domainLinks = []
+    domain = urlparse(self.url)[1]
+    for link in links: # select the url in href for all a tags(links)
+      parsedLink = urlparse(link)
+      if(parsedLink[1] == '' or parsedLink[1] == domain): # No or same domain
+        domainLinks.append(link)
+    return domainLinks
+
 
   def getFiles(self, max=50):
     return
--- scrapython.py
+++ scrapython.py
@@ -1,15 +1,14 @@
 import sys, string, urllib2
 import Page
 
-
 if __name__ == '__main__':
   if(len(sys.argv)) > 1:
     url = sys.argv[1]
 
     f = urllib2.urlopen(url)
     content = f.read()
-    myPage = Page.Page(content, 'index.html', '/')
+    myPage = Page.Page(content, url)
 
-    print myPage.getUrl()
+    print myPage.getDomainLinks()
   else:
-    print "No arguments found"
+    print "No argument found"
